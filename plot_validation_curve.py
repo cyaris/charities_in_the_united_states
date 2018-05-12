153499ff16a7106a679ad5f3f527d22db734fc1f
cyaris <charlieyaris@gmail.com>
2018-05-11 19:12:11 -0700

    Added and modified sci-kit learn learning curve and validation curve plotting modeules.

---
+++ plot_validation_curve.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import validation_curve
+
+def plot_validation_curve(estimator, param_name, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    plt.figure(figsize = (7, 5))
+    param_range = np.logspace(-6, -1, 5)
+    train_scores, test_scores = validation_curve(
+    estimator, X, y, param_name, param_range=param_range,
+    cv=3, scoring="accuracy", n_jobs=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.title(title)
+    plt.xlabel(param_name)
+    plt.ylabel("Score")
+    # plt.ylim(0, 1)
+    lw = 2
+    plt.semilogx(param_range, train_scores_mean, label="Training score",
+                 color="g", lw=lw)
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.2,
+                     color="darkgreen", lw=lw)
+    plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+                 color="b", lw=lw)
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.2,
+                     color="darkblue", lw=lw)
+    plt.legend(loc="best")
+    plt.show()
